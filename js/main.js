acae55302fd570b7a89ca183e9c2505233dccc6a
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-01-12 03:21:09 -0300

    feat: :sparkles: add: sound togle playing music

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ const longoBtn = document.querySelector(".app__card-button--longo");
 const botoes = document.querySelectorAll(".app__card-button");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
+const inputCheckMusic = document.querySelector("#alternar-musica");
+const musica = new Audio("/sound/luna-rise-part-one.mp3");
+musica.loop = true;
+
+inputCheckMusic.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
 focoBtn.addEventListener("click", () => {
   alterarContexto("foco");
