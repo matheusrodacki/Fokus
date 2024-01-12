42a373ee124ea0aba1f7b7eb00fae5b49c72c788
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-01-12 02:51:01 -0300

    refactor: :recycle: refactor: add alterarContexto() function

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const html = document.querySelector("html");
+const focoBtn = document.querySelector(".app__card-button--foco");
+const curtoBtn = document.querySelector(".app__card-button--curto");
+const longoBtn = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+
+focoBtn.addEventListener("click", () => {
+  alterarContexto("foco");
+});
+
+curtoBtn.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+});
+
+longoBtn.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+});
+
+function alterarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/img/${contexto}.png`);
+}
